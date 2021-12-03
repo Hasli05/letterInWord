d9b3710b61d08801a86ab70fb9937251fabfb857
Max <msxkurko3@gmail.com>
2021-12-03 15:40:57 +0200

    Добавьте файлы проекта.

---
+++ letterInString.cpp
@@ -0,0 +1,26 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+    string userWord;
+    char userLetter;
+    int letterNum = 0;
+
+    cout << "Enter word :" << endl;
+    cin >> userWord;
+
+    cout << "Enter letter :" << endl;
+    cin >> userLetter;
+
+    for (int i = 0; i < userWord.size(); ++i)
+    {
+        if (userLetter == userWord[i])
+        {
+            ++letterNum;
+        }
+    }
+
+    cout << "Letter " << userLetter << " in word :" << letterNum << endl;
+}
+
